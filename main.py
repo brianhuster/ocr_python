2f6a38e0f2cd81eb5ba05e2ba164c4ae6714dad8
brianhuster <phambinhanctb2004@gmail.com>
2024-04-28 00:55:23 +0700

    update

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from vietocr.tool.predictor import Predictor
 from vietocr.tool.config import Cfg
 import traceback
 import os
+import argparse
+import urllib.request
 
 pytesseract.pytesseract.tesseract_cmd = r'F:\Tesseract-OCR\tesseract' #replace it with the actual path to your tesseract file. If you are using MacOS or Linux, remove this line
 text_corrector = Corrector(device='cpu', model_type='seq2seq', weight_path='./weights/seq2seq_0.pth')
@@ -22,25 +24,16 @@ config['cnn']['pretrained']=False
 config['predictor']['beamsearch']=False
 OCRpredictor = Predictor(config)
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-command=None
-
 def listen_for_command():
     global command
     r = sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
-        print("Google is listening for command...")
+        print("\nGoogle is listening for command...\n")
         audio = r.listen(source)
     try:
-        # command = r.recognize_whisper(audio, language='vietnamese')
         command = r.recognize_google(audio, language='vi-VN')
-        # command=r.recognize_vosk(audio, language='vi-VN')
-        print(f"Command received: {command}")
+        print(f"\nCommand received: {command}\n")
         return command
     except Exception as e:
         print(f"Error: {str(e)}")
@@ -63,11 +56,11 @@ def preprocess_image(image):
 
     # Convert to grayscale
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite("Gray.jpg", image)
 
-    # # Apply binarization
-    # _, image = cv2.threshold(image, 240, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-    # cv2.imwrite("Binary.jpg", image)
+    # Apply binarization
+    _, image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    cv2.imshow("Binary", image)
+    cv2.waitKey(0)
 
     # # Contrast enhancement
     # image = cv2.equalizeHist(image)
@@ -88,10 +81,14 @@ def preprocess_image(image):
     return image
 
 def OCR(image):
+    t=time.time()
     d = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
+    print(d['text'])
+    print("\nDetecting time: ", time.time()-t)
+    t=time.time()
     n_boxes = len(d['text'])
     if n_boxes == 0:
-        print("No text detected")
+        print("\nNo text detected\n")
         return
     text = ""
     last_y = d['top'][0]
@@ -108,11 +105,17 @@ def OCR(image):
             last_y = y
             last_h = h
             text += cut_text + " "
+    print("\nRecognization time: ", time.time()-t)
     return text
 
 def text_correction(text):
     try:
-        return text_corrector.predict(text.strip(), NGRAM=6)
+        print("\nRecognized text: ", text)
+        t=time.time()
+        text=text_corrector.predict(text.strip(), NGRAM=6)
+        print("\nCorrected text: ", text)
+        print("\nCorrection time: ", time.time()-t)
+        return text
     except Exception as e:
         print(f"Error: {str(e)}")
         return text
@@ -123,38 +126,66 @@ def speak_gtts(text):
     playsound("temp.mp3")
     os.remove("temp.mp3")
 
-thread=threading.Thread(target=listen_for_command)
-thread.start()
+parser = argparse.ArgumentParser(description='Read text from an image or camera. You can only provide either --ImagePath or --CameraSource, not both')
+parser.add_argument('-i', '--ImagePath', type=str, help='Path to an image')
+parser.add_argument('-c', '--CameraSource', type=str, help='Index or URL of the camera to use. Default is 0')
+args = parser.parse_args()
 
-while True:
-    ret, frame = cap.read()
-    cv2.imshow("Camera", frame)
-    t=time.time()
-    if command is not None:
-        if "đọc chữ" in command.lower():
-            recognized_text = OCR(frame)
-            cv2.imshow("Camera", frame)
-            if recognized_text.strip():
-                print("Văn bản chưa sửa lỗi: ", recognized_text)
-                try:
+if args.ImagePath is not None and args.CameraSource is not None:
+    parser.error('You can only provide either --ImagePath or --CameraSource, not both')
+if args.ImagePath is None and args.CameraSource is None:
+    args.CameraSource = '0'
+
+if args.ImagePath:
+    if args.ImagePath.startswith('http://') or args.ImagePath.startswith('https://'):
+        resp = urllib.request.urlopen(args.ImagePath)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    else:
+        image = cv2.imread(args.ImagePath)
+    recognized_text = OCR(image)
+    if recognized_text.strip():
+        recognized_text = text_correction(recognized_text)
+        speak_gtts(recognized_text)
+    exit()
+
+if args.CameraSource:
+    if args.CameraSource.isdigit():
+        cap = cv2.VideoCapture(int(args.CameraSource))
+    else:
+        cap = cv2.VideoCapture(args.CameraSource)
+    cap.set(3, 640)
+    cap.set(4, 560)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    command=None
+
+    thread=threading.Thread(target=listen_for_command)
+    thread.start()
+
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow("Camera", frame)
+        t=time.time()
+        if command is not None:
+            if "đọc chữ" in command.lower():
+                recognized_text = OCR(frame)
+                cv2.imshow("Camera", frame)
+                if recognized_text.strip():
                     recognized_text = text_correction(recognized_text)
-                    print("Văn bản đã sửa lỗi: ", recognized_text)
-                except Exception as e:
-                    print(f"Error: {str(e)}")
-                    traceback.print_exc()
-                speak_thread = threading.Thread(target=speak_gtts, args=(recognized_text,))
-                speak_thread.start()
-                speak_thread.join()
-            command = None
-        else:
-            if not thread.is_alive():
-                thread=threading.Thread(target=listen_for_command)
-                thread.start()
-    if cv2.getWindowProperty('Camera', cv2.WND_PROP_VISIBLE) < 1:  
-        break
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-    
-cap.release()
-cv2.destroyAllWindows()
+                    speak_thread = threading.Thread(target=speak_gtts, args=(recognized_text,))
+                    speak_thread.start()
+                    speak_thread.join()
+                command = None
+            else:
+                if not thread.is_alive():
+                    thread=threading.Thread(target=listen_for_command)
+                    thread.start()
+        if cv2.getWindowProperty('Camera', cv2.WND_PROP_VISIBLE) < 1:  
+            break
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
 
