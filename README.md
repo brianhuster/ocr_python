e56cd906f26bddb7eff7885e137a3e1dbcd5972c
brianhuster <phambinhanctb2004@gmail.com>
2024-04-27 23:27:21 +0700

    update

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-﻿# read_text_from_camera
+﻿﻿# read_text_from_camera
 Code Python đọc văn bản tiếng Việt từ video camera và chuyển thành giọng nói
 ## Cài đặt
 ```git clone https://github.com/brianhuster/read_text_from_camera/```
 
 ```cd read_text_from_camera```
 ```pip install -r requirements.txt```
-#Ubuntu
+### Ubuntu
 ```
 sudo apt-get update
 sudo apt-get install espeak
@@ -15,10 +15,23 @@ sudo apt-get install -y libgl1-mesa-glx
 sudo apt-get install tesseract-ocr-vie
 sudo apt-get install -y portaudio19-dev python3-pyaudio
 ```
-
+### Windows
+Install Tesseract [here](https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-5.3.3.20231005.exe) and replace ```F:\Tesseract-OCR\tesseract``` in the file main.py with your actual path to Tesseract
 ### Chạy chương trình
 ```python3 main.py```
 
 Người dùng ra lệnh, sau đó chương trình sẽ nghe chuyển giọng nói người dùng thành văn bản qua dịch vụ của Google và hiển thị câu lệnh trên Terminal. 
 
 Nếu câu lệnh của người dùng chứa từ "đọc chữ" thì chương trình sẽ chụp hình từ camera rồi chuyển chữ thành văn bản trên terminal, rồi đọc to văn bản qua dịch vụ chuyển văn bản thành giọng nói của Google
+
+Khi video camera đang chạy, nhấn phím ```q``` để tắt
+
+### Video demo
+[![Demo CountPages alpha](https://i9.ytimg.com/vi/v9YPcHzfTyk/mqdefault.jpg?sqp=CNjYrrEG-oaymwEmCMACELQB8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgQyhCMA8=&rs=AOn4CLCxICP7zv-lwkr3xoMfB8t1JQ5alw)](https://www.youtube.com/watch?v=v9YPcHzfTyk)
+
+## References
+[https://github.com/madmaze/pytesseract](https://github.com/madmaze/pytesseract)
+
+[https://github.com/pbcquoc/vietocr](https://github.com/pbcquoc/vietocr)
+
+[https://github.com/buiquangmanhhp1999/VietnameseOcrCorrection](https://github.com/buiquangmanhhp1999/VietnameseOcrCorrection)
--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 import cv2
-import pytesseract
+from PIL import Image
 import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 import threading
-import pyttsx3
 import numpy as np
 import time
-from tool.predictor import Predictor
+from tool.predictor import Predictor as Corrector
+import pytesseract
+from vietocr.tool.predictor import Predictor
+from vietocr.tool.config import Cfg
 import traceback
 import os
 
 pytesseract.pytesseract.tesseract_cmd = r'F:\Tesseract-OCR\tesseract' #replace it with the actual path to your tesseract file. If you are using MacOS or Linux, remove this line
-model_predictor = Predictor(device='cpu', model_type='seq2seq', weight_path='./weights/seq2seq_0.pth')
-pyttsx3_engine = pyttsx3.init()
-pyttsx3_engine.setProperty('voice', 'vietnam')
-pyttsx3_engine.setProperty('rate', 145)
+text_corrector = Corrector(device='cpu', model_type='seq2seq', weight_path='./weights/seq2seq_0.pth')
+config = Cfg.load_config_from_name('vgg_transformer')
+config['weights'] = 'https://huggingface.co/brianhuster/VietnameseOCR/resolve/main/vgg_transformer.pth'
+config['device'] = 'cpu'
+config['cnn']['pretrained']=False
+config['predictor']['beamsearch']=False
+OCRpredictor = Predictor(config)
 
-cap = cv2.VideoCapture('http://192.168.1.3:8080/video')
+cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 command=None
-def text_correction(text):
+
+def listen_for_command():
+    global command
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print("Google is listening for command...")
+        audio = r.listen(source)
     try:
-        return model_predictor.predict(text.strip(), NGRAM=6)
+        # command = r.recognize_whisper(audio, language='vietnamese')
+        command = r.recognize_google(audio, language='vi-VN')
+        # command=r.recognize_vosk(audio, language='vi-VN')
+        print(f"Command received: {command}")
+        return command
     except Exception as e:
         print(f"Error: {str(e)}")
-        return text
+        traceback.print_exc()
+        return None
 
 def preprocess_image(image):
     # Resize (optional)
@@ -43,21 +60,6 @@ def preprocess_image(image):
     # # Contrast stretching
     # image = cv2.normalize(image, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8UC1)
     # cv2.imwrite("Contrast stretching.jpg", image)
-
-    # # Sharpening
-    # kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], np.float32)
-    # image = cv2.filter2D(image, -1, kernel=kernel)
-    # cv2.imwrite("Sharpening.jpg", image)
-
-    # # Brightness adjustment
-    # image = cv2.convertScaleAbs(image, alpha=1, beta=5)
-    # cv2.imwrite("Brightness adjustment.jpg", image)
-
-    # # Gamma correction
-    # gamma = 1.5
-    # lookup_table = np.array([((i / 255.0) ** gamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
-    # image = cv2.LUT(image, lookup_table)
-    # cv2.imwrite("Gamma correction.jpg", image)
 
     # Convert to grayscale
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -85,32 +87,41 @@ def preprocess_image(image):
     # cv2.imwrite("Rotated.jpg", image)
     return image
 
-def listen_for_command():
-    global command
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-        print("Google is listening for command...")
-        audio = r.listen(source)
+def OCR(image):
+    d = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
+    n_boxes = len(d['text'])
+    if n_boxes == 0:
+        print("No text detected")
+        return
+    text = ""
+    last_y = d['top'][0]
+    last_h = d['height'][0]
+    for i in range(n_boxes):
+        if int(d['conf'][i]) > 0:
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            cut = image[y:y+h, x:x+w]
+            cut = preprocess_image(cut)
+            cv2.waitKey(0)
+            cut_text = OCRpredictor.predict(img = Image.fromarray(cut))
+            if y - last_y > last_h:
+                text += "\n"
+            last_y = y
+            last_h = h
+            text += cut_text + " "
+    return text
+
+def text_correction(text):
     try:
-        # command = r.recognize_whisper(audio, language='vietnamese')
-        command = r.recognize_google(audio, language='vi-VN')
-        # command=r.recognize_vosk(audio, language='vi-VN')
-        print(f"Command received: {command}")
-        return command
+        return text_corrector.predict(text.strip(), NGRAM=6)
     except Exception as e:
         print(f"Error: {str(e)}")
-        traceback.print_exc()
-        return None
+        return text
 
 def speak_gtts(text):
     tts = gTTS(text=text, lang='vi')
     tts.save("temp.mp3")
-    playsound(os.path.dirname(__file__) + "\\temp.mp3")
-
-def speak_pyttsx3(text):
-    pyttsx3_engine.say(text)
-    pyttsx3_engine.runAndWait()
+    playsound("temp.mp3")
+    os.remove("temp.mp3")
 
 thread=threading.Thread(target=listen_for_command)
 thread.start()
@@ -119,22 +130,9 @@ while True:
     ret, frame = cap.read()
     cv2.imshow("Camera", frame)
     t=time.time()
-    if command is not None and "đọc chữ" in command.lower():
-        print("Đang tiền xử lý ảnh")
-        frame=preprocess_image(frame)
-        print("Bắt đầu đọc văn bản")
-        recognized_text = pytesseract.image_to_string(frame, lang='vie')
-        d = pytesseract.image_to_data(frame, output_type=pytesseract.Output.DICT)
-        text=d['text']
-        n_boxes = len(d['text'])
-        if n_boxes>0:
-            print(recognized_text)
-            for i in range(n_boxes):
-                if int(d['conf'][i]) > 0:
-                    (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-                    frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            frame = cv2.putText(frame, recognized_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            cv2.imwrite("frame with text.jpg", frame)
+    if command is not None:
+        if "đọc chữ" in command.lower():
+            recognized_text = OCR(frame)
             cv2.imshow("Camera", frame)
             if recognized_text.strip():
                 print("Văn bản chưa sửa lỗi: ", recognized_text)
@@ -147,11 +145,11 @@ while True:
                 speak_thread = threading.Thread(target=speak_gtts, args=(recognized_text,))
                 speak_thread.start()
                 speak_thread.join()
-        command = None
-    else:
-        if not thread.is_alive():
-            thread=threading.Thread(target=listen_for_command)
-            thread.start()
+            command = None
+        else:
+            if not thread.is_alive():
+                thread=threading.Thread(target=listen_for_command)
+                thread.start()
     if cv2.getWindowProperty('Camera', cv2.WND_PROP_VISIBLE) < 1:  
         break
     if cv2.waitKey(1) & 0xFF == ord("q"):
