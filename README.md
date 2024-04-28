8faf82a3c029b271f52a45822a90e0715a7d3a8f
brianhuster <phambinhanctb2004@gmail.com>
2024-04-28 21:44:18 +0700

    update

--- README.md
+++ README.md
@@ -1,33 +1,44 @@
 ﻿﻿# read_text_from_camera
 Code Python đọc văn bản tiếng Việt từ video camera và chuyển thành giọng nói
 ## Cài đặt
-```git clone https://github.com/brianhuster/read_text_from_camera/```
-
-```cd read_text_from_camera```
-```pip install -r requirements.txt```
-### Ubuntu
+```shell
+git clone https://github.com/brianhuster/read_text_from_camera/
+cd read_text_from_camera
+pip install -r requirements.txt
 ```
-sudo apt-get update
-sudo apt-get install espeak
-sudo apt-get install libgirepository1.0-dev
-sudo apt-get install libcairo2-dev
-sudo apt-get install -y libgl1-mesa-glx
-sudo apt-get install tesseract-ocr-vie
-sudo apt-get install -y portaudio19-dev python3-pyaudio
+
+## Usage
+```shell
+python3 script.py [-h] [-i IMAGEPATH] [-c CAMERASOURCE] [-d {easyocr,tesseract}] [-r {easyocr,easy_ocr,vietocr}] [-l LANGUAGE] [-t {y,n}]
 ```
-### Windows
-Install Tesseract [here](https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-5.3.3.20231005.exe) and replace ```F:\Tesseract-OCR\tesseract``` in the file main.py with your actual path to Tesseract
-### Chạy chương trình
-```python3 main.py```
 
-Người dùng ra lệnh, sau đó chương trình sẽ nghe chuyển giọng nói người dùng thành văn bản qua dịch vụ của Google và hiển thị câu lệnh trên Terminal. 
+Arguments:
+- `-i IMAGEPATH`, `--ImagePath IMAGEPATH`: Local path or URL to an image
+- `-c CAMERASOURCE`, `--CameraSource CAMERASOURCE`: Index or URL of the video camera (default: 0)
+- `-d {easyocr,tesseract}`, `--detector {easyocr,tesseract}`: Detector to use (default: easyocr)
+- `-r {easyocr,easy_ocr,vietocr}`, `--recognizer {easyocr,easy_ocr,vietocr}`: Recognizer to use (default: easyocr)
+- `-l LANGUAGE`, `--language LANGUAGE`: 2-letter code of language to use for OCR (default: vi)
+- `-t {y,n}`, `--textCorrector {y,n}`: Use text corrector or not (y/n) for Vietnamese text (default: n)
 
-Nếu câu lệnh của người dùng chứa từ "đọc chữ" thì chương trình sẽ chụp hình từ camera rồi chuyển chữ thành văn bản trên terminal, rồi đọc to văn bản qua dịch vụ chuyển văn bản thành giọng nói của Google
-<<<<<<< HEAD
+### Examples
 
-Khi video camera đang chạy, nhấn phím ```q``` để tắt
+1. Recognize text from an image file:
+```shell
+python3 script.py -i image.jpg
+```
+2. Recognize text from a camera feed:
+```shell
+python script.py -c 0
+```
+or 
+```shell
+python3 script.py
+```
+Say "đọc chữ" or "read it" to trigger text recognition and speech synthesis.
 
-### Video demo
+While the camera is running, press ```q``` to turn it off
+
+### Demo video
 [![Demo CountPages alpha](https://i9.ytimg.com/vi/v9YPcHzfTyk/mqdefault.jpg?sqp=CNjYrrEG-oaymwEmCMACELQB8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgQyhCMA8=&rs=AOn4CLCxICP7zv-lwkr3xoMfB8t1JQ5alw)](https://www.youtube.com/watch?v=v9YPcHzfTyk)
 
 ## References
--- main.py
+++ main.py
@@ -17,6 +17,10 @@ import urllib.request
 import easyocr
 import platform
 import subprocess
+import warnings
+import pycountry
+import pkg_resources
+import sys
 
 def listen_for_command():
     global command
@@ -40,9 +44,8 @@ def run_shell_command(command):
     return output.decode(), error.decode()
 
 def preprocess_image(image):
-    # Resize (optional)
+    # Resize
     image = cv2.resize(image, None, fx=2.0, fy=2.0, interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite("Resized.jpg", image)
 
     # # Apply noise removal
     # kernel = np.ones((3, 3), np.uint8)
@@ -78,165 +81,206 @@ def preprocess_image(image):
     # cv2.imwrite("Rotated.jpg", image)
     return image
 
-def OCR1(image): # detect using tesseract
-    t=time.time()
+def detect_tesseract(image): # detect using tesseract
     d = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
-    print("\nDetecting time: ", time.time()-t)
-    t=time.time()
     n_boxes = len(d['text'])
     if n_boxes == 0:
         print("\nNo text detected\n")
         return
-    text = ""
-    last_y = d['top'][0]
-    last_h = d['height'][0]
+    array=[]
     for i in range(n_boxes):
         if int(d['conf'][i]) > 0:
-            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-            cut = image[y:y+h, x:x+w]
-            cut = preprocess_image(cut)
-            cv2.waitKey(0)
-            cut_text = OCRpredictor.predict(img = Image.fromarray(cut))
-            if y - last_y > last_h:
-                text += "\n"
-            last_y = y
-            last_h = h
-            text += cut_text + " "
-    print("\nRecognization time: ", time.time()-t)
-    return text
+            array.append((d['top'][i], d['top'][i]+d['height'][i], d['left'][i], d['left'][i]+d['width'][i], d['text'][i]))
+    return array
 
-def OCR2(image): # detect using easyocr
+def detect_easyocr(image): # detect using easyocr
     t=time.time()
     d=reader.readtext(image)
-    print("\nDetecting time: ", time.time()-t)
     t=time.time()
     n_boxes = len(d)
     if n_boxes == 0:
         print("\nNo text detected\n")
         return
-    text = ""
-    last_bottom=d[0][0][2][1]
+    array=[]
     for i in range(n_boxes):
         if d[i][2] > 0:
-            (top, bottom, left, right) = (d[i][0][0][1], d[i][0][2][1], d[i][0][0][0], d[i][0][1][0])
-            print(top, bottom, left, right)
-            cut = image[int(top):int(bottom), int(left):int(right)]
-            cut = preprocess_image(cut)
-            cv2.waitKey(0)
-            cut_text = OCRpredictor.predict(img = Image.fromarray(cut))
-            if top>last_bottom:
-                text += "\n"
-            last_bottom = bottom
-            text += cut_text + " "
-    print("\nRecognization time: ", time.time()-t)
+            (top, bottom, left, right, text) = (d[i][0][0][1], d[i][0][2][1], d[i][0][0][0], d[i][0][1][0], d[i][1])
+            array.append((top, bottom, left, right, text))
+    return array
+
+def recognize(image, array):
+    text=""
+    last_bottom=array[0][1]
+    for box in array:
+        cut = image[int(box[0]):int(box[1]), int(box[2]):int(box[3])]
+        cut = preprocess_image(cut)
+        if args.recognizer == args.detector:
+            cut_text=box[4]
+        elif args.recognizer == 'easy_ocr':
+            cut_text = '\n'.join(reader.readtext(cut, detail=0, paragraph=True))
+        elif args.recognizer == 'vietocr':
+            cut_text = vietocr_predictor.predict(img = Image.fromarray(cut))
+        elif args.recognizer == 'tesseract':
+            cut_text = pytesseract.image_to_string(cut, lang='vie')
+        if box[0]>last_bottom:
+            text += "\n"
+        last_bottom = box[1]
+        text += cut_text + " "
     return text
 
-def OCR(image):
-    if detector == 'easyocr':
-        return OCR2(image)
-    elif detector == 'tesseract':
-        return OCR1(image)
-
 def text_correction(text):
+    if not text:
+        return text
     try:
-        print("\nRecognized text: ", text)
         t=time.time()
         text=text_corrector.predict(text.strip(), NGRAM=6)
-        print("\nCorrected text: ", text)
-        print("\nCorrection time: ", time.time()-t)
         return text
     except Exception as e:
-        print(f"Error: {str(e)}")
         return text
 
+def OCR(image):
+    t=time.time()
+    if args.detector == 'easyocr':
+        array = detect_easyocr(image)
+    elif args.detector == 'tesseract':
+        array = detect_tesseract(image)
+    if array is None:
+        return
+    text=recognize(image, array)
+    if args.textCorrector == 'y' and args.language == 'vi' and text.strip():
+        text=text_correction(text)
+    print("Recognized text :", text)
+    print(f"Time taken to process: {time.time()-t:.2f} seconds")
+    return text
+
 def speak_gtts(text):
-    tts = gTTS(text=text, lang='vi')
+    tts = gTTS(text=text, lang=args.language)
     tts.save("temp.mp3")
     playsound("temp.mp3")
     os.remove("temp.mp3")
 
-parser = argparse.ArgumentParser(description='Read text from an image or camera. You can not provide both --ImagePath and --CameraSource at the same time.')
-parser.add_argument('-i', '--ImagePath', type=str, help='Path to an image')
-parser.add_argument('-c', '--CameraSource', type=str, help='Index or URL of the camera to use. Default is 0')
-if platform.system() != 'Windows':
-    parser.add_argument('-d', '--detector', type=str, default='easyocr', help='Detector to use, "tesseract" or "easyocr". Default is "easyocr"')
-args = parser.parse_args()
-
-if platform.system() == 'Windows' or args.detector == 'easyocr':
-    reader = easyocr.Reader(['vi','en'])
-    detector='easyocr'
-else:
-    detector='tesseract'
-    reader=None
-    print("\nCHECKING AND INSTALLING TESSERACT (if not found)\n")
-    if platform.system() == 'Linux':
-        run_shell_command("sudo apt-get install tesseract-ocr-vie")
-        run_shell_command("sudo apt-get install tesseract-ocr-script-viet")
-    elif platform.system() == 'Darwin':
-        run_shell_command("sudo port install tesseract-vie")
-
-text_corrector = Corrector(device='cpu', model_type='seq2seq', weight_path='./weights/seq2seq_0.pth')
-config = Cfg.load_config_from_name('vgg_transformer')
-config['weights'] = 'https://huggingface.co/brianhuster/VietnameseOCR/resolve/main/vgg_transformer.pth'
-config['device'] = 'cpu'
-config['cnn']['pretrained']=False
-config['predictor']['beamsearch']=False
-OCRpredictor = Predictor(config)
-
-if args.ImagePath is not None and args.CameraSource is not None:
-    parser.error('You can not provide both --ImagePath and --CameraSource at the same time.')
-if args.ImagePath is None and args.CameraSource is None:
-    args.CameraSource = '0'
-
-if args.ImagePath:
-    if args.ImagePath.startswith('http://') or args.ImagePath.startswith('https://'):
-        resp = urllib.request.urlopen(args.ImagePath)
-        image = np.asarray(bytearray(resp.read()), dtype="uint8")
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Read text from image(s) or camera. Among  --ImagePath (-i), imageFolders (-f) and --CameraSource (-c), you can provide ONLY ONE option for a runtime.')
+    parser.add_argument('-i', '--ImagePath', type=str, help='Local path or URL to an image')
+    parser.add_argument('-f', '--imagesFolder', type=str, help='Local path to a folder containing images. This will process all images in the folder and save recognized text in txt files the same folder.')
+    parser.add_argument('-c', '--CameraSource', type=str, help='Index or URL of the video camera to use. Default is 0')
+    if platform.system() != 'Windows':
+        parser.add_argument('-d', '--detector', type=str, default='easyocr', help='Detector to use, "tesseract" or "easyocr". Default is "easyocr"')
+        parser.add_argument('-r', '--recognizer', type=str, default='easyocr', help='Recognizer to use, "easy_ocr" or "vietocr". Default is "easy_ocr"')
     else:
-        image = cv2.imread(args.ImagePath)
-    recognized_text = OCR(image)
-    if recognized_text.strip():
-        recognized_text = text_correction(recognized_text)
-        speak_gtts(recognized_text)
-    exit()
+        parser.add_argument('-d', '--detector', type=str, default='easyocr', help='Currently only "easyocr" is supported.')
+        parser.add_argument('-r', '--recognizer', type=str, default='easyocr', help='Recognizer to use, "easy_ocr", "vietocr" or "tesseract". Default is "easy_ocr"')
+    parser.add_argument('-l', '--language', type=str, default='vi', help='2-letter code of language to use for OCR. Default is "vi" (Vietnamese)')
+    parser.add_argument('-t', '--textCorrector', type=str, default='n', help='Use text corrector or not (y/n). This only works for Vietnamese. Default is "n", however, we recommend using it if you are trying to recognize Vietnamese text.')
+    parser.add_argument('-s', '--TTS', type=str, default='y', help='Use text-to-speech or not (y/n). Default is "y"')
+    args = parser.parse_args()
 
-if args.CameraSource:
-    if args.CameraSource.isdigit():
-        cap = cv2.VideoCapture(int(args.CameraSource))
-    else:
-        cap = cv2.VideoCapture(args.CameraSource)
-    cap.set(3, 640)
-    cap.set(4, 560)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    command=None
+    if args.recognizer == 'easyocr' or args.detector == 'easyocr':
+        reader = easyocr.Reader([args.language,'en'])
 
-    thread=threading.Thread(target=listen_for_command)
-    thread.start()
+    if args.recognizer == 'tesseract' or args.detector == 'tesseract':
+        if platform.system() == 'Windows':
+            parser.error("Sorry, Tesseract is not supported on Windows")
+        print("\nCHECKING AND INSTALLING TESSERACT (if not found)\n")
+        if platform.system() == 'Linux':
+            run_shell_command("sudo apt-get install tesseract-ocr-vie")
+            run_shell_command("sudo apt-get install tesseract-ocr-script-viet")
+        elif platform.system() == 'Darwin':
+            run_shell_command("sudo port install tesseract-vie")
 
-    while True:
-        ret, frame = cap.read()
-        cv2.imshow("Camera", frame)
-        t=time.time()
-        if command is not None:
-            if "đọc chữ" in command.lower():
-                recognized_text = OCR(frame)
-                cv2.imshow("Camera", frame)
+    if args.recognizer == 'vietocr':
+        if args.language != 'vi':
+            parser.error("VietOCR only supports Vietnamese language.")
+        config = Cfg.load_config_from_name('vgg_transformer')
+        config['weights'] = 'https://huggingface.co/brianhuster/VietnameseOCR/resolve/main/vgg_transformer.pth'
+        config['device'] = 'cpu'
+        config['cnn']['pretrained']=False
+        config['predictor']['beamsearch']=False
+        vietocr_predictor = Predictor(config)
+
+    if args.textCorrector == 'y':
+        text_corrector = Corrector(device='cpu', model_type='seq2seq', weight_path=('weights/seq2seq_0.pth'))
+
+    if args.language != 'vi' and args.textCorrector == 'y':
+        warnings.warn("Text corrector only works for Vietnamese, so it will be disabled.")
+
+    if (args.ImagePath is not None and args.CameraSource is not None) or (args.folderPath is not None and args.CameraSource is not None) or (args.folderPath is not None and args.ImagePath is not None):
+        parser.error('You can provide ONLY ONE of these 3 options, either --ImagePath (-i), imageFolders (-f) or --CameraSource (-c) for a runtime.')
+    if args.ImagePath is None and args.CameraSource is None:
+        args.CameraSource = '0'
+
+    #start processing
+
+    if args.ImagePath:
+        if args.ImagePath.startswith('http://') or args.ImagePath.startswith('https://'):
+            try:
+                resp = urllib.request.urlopen(args.ImagePath)
+                image = np.asarray(bytearray(resp.read()), dtype="uint8")
+                image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+            except urllib.error.HTTPError as e:
+                if e.code == 403:
+                    sys.exit("HTTP Error 403: Forbidden. The URL provided doesn't allow me to download the image. You can manually download the image and provide the local path to it.")
+            except Exception as e:
+                sys.exit(f"Error: {str(e)}")
+        else:
+            image = cv2.imread(args.ImagePath)
+        recognized_text = OCR(image)
+        if args.TTS=='y' and recognized_text.strip():
+            speak_gtts(recognized_text)
+        sys.exit()
+
+    if args.imagesFolder:
+        if not os.path.exists(args.imagesFolder):
+            sys.exit(f"Error: Folder {args.imagesFolder} not found")
+        for file in os.listdir(args.imagesFolder):
+            image = cv2.imread(os.path.join(args.imagesFolder, file))
+            if image is not None:
+                recognized_text = OCR(image)
                 if recognized_text.strip():
-                    recognized_text = text_correction(recognized_text)
-                    speak_thread = threading.Thread(target=speak_gtts, args=(recognized_text,))
-                    speak_thread.start()
-                    speak_thread.join()
-                command = None
-            else:
-                if not thread.is_alive():
-                    thread=threading.Thread(target=listen_for_command)
-                    thread.start()
-        if cv2.getWindowProperty('Camera', cv2.WND_PROP_VISIBLE) < 1:  
-            break
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-        
-    cap.release()
-    cv2.destroyAllWindows()
+                    print(f"\nRecognized text from {file}:\n{recognized_text}\n")
+                    txt_file_name = file + '.txt'
+                    with open(os.path.join(args.imagesFolder, txt_file_name), 'w') as f:
+                        f.write(recognized_text)
+                    if args.TTS=='y':
+                        speak_gtts(recognized_text)
+                    
+        sys.exit()
+
+    elif args.CameraSource:
+        if args.CameraSource.isdigit():
+            cap = cv2.VideoCapture(int(args.CameraSource))
+        else:
+            cap = cv2.VideoCapture(args.CameraSource)
+        cap.set(3, 640)
+        cap.set(4, 560)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        command=None
+
+        thread=threading.Thread(target=listen_for_command)
+        thread.start()
+
+        while True:
+            ret, frame = cap.read()
+            cv2.imshow("Camera", frame)
+            t=time.time()
+            if command is not None:
+                if "đọc chữ" in command.lower():
+                    recognized_text = OCR(frame)
+                    cv2.imshow("Camera", frame)
+                    if recognized_text.strip():
+                        speak_thread = threading.Thread(target=speak_gtts, args=(recognized_text,))
+                        speak_thread.start()
+                        speak_thread.join()
+                    command = None
+                else:
+                    if not thread.is_alive():
+                        thread=threading.Thread(target=listen_for_command)
+                        thread.start()
+            if cv2.getWindowProperty('Camera', cv2.WND_PROP_VISIBLE) < 1:  
+                break
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+            
+        cap.release()
+        cv2.destroyAllWindows()
